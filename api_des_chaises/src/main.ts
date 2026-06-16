8cd7c0e3b3128518339abe3a2d529e4c7e8b30c2
Loïs Marteel <marteellois@gmail.com>
2026-06-16 13:17:42 +0200

    main pour local

--- api_des_chaises/src/main.ts
+++ api_des_chaises/src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    // origin: process.env.FRONTEND_URL || 'http://localhost:8080', // port de Next.js
-    origin: 'https://vonaman.fr',
+    origin: process.env.FRONTEND_URL || 'http://localhost:8080', // port de Next.js
+    //origin: 'https://vonaman.fr',
     credentials: true,
   });
 
